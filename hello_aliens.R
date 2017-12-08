f6ba0520b95b365b282166a0509b5139baf07c60
Gunho Jung <33228830+gunhojung@users.noreply.github.com>
2017-12-08 03:09:40 -0500

    hello_aliens.R

---
+++ hello_aliens.R
@@ -0,0 +1,12 @@
+cat("Hi! 
+My name is Gunho Jung who has recently been selected as the one to write a message to you guys.
+Honestly, I am not greatly familiar with your world and existence, but if you get to see my message, I would like to tell you that we are very interested in you.
+We are very curious about your existence and how we can cooperate with each other for mutual enhancement in technology.
+Also, globally, we human beings share similar interest about the whole universe, so if you guys want to know more about us, please respond to this message.
+We are uncertain when you will receive this message and when you will send the message to us, but we look forward to have interaction with you.
+Human beings are very nice and therefore feel free to give us signal that you are approaching to Earth.
+Thanks
+
+Best,
+
+Gunho ")
